06408c2333dafb9127710a208909d1f9a06acb70
DimitriChrysafis <d4c@ohs.stanford.edu>
2022-11-16 20:56:40 -0800

    Add files via upload

---
+++ WordleEventListener.java
@@ -0,0 +1,10 @@
+public interface WordleEventListener {
+
+    /*
+     * This function defines WordleEventListener as a functional interface,
+     * suitable for use with arrow functions.
+     */
+
+    public void eventAction(String s);
+
+}
